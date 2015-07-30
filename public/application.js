7127048b82ff62542723d65b6e7720190fe7e143
Harry Chen <harrychenca@gmail.com>
2015-07-30 14:38:51 +0800

    feat(a lot): Auth, Sessions, Users

--- public/application.js
+++ public/application.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// $.ajax({
+//   type: 'POST',
+//   url: 'users',
+//   data: { // this is the payload
+//     user: {
+//       username: 'asdasdasd',
+//       name: 'asdasdasd',
+//       email: 'asd@asd.com',
+//       password: 'asdasdasd'
+//     }
+//   },
+//   dataType: 'JSON',
+//   success: function(response, status){
+//     console.log('response: ', response);
+//   },
+//   error: function(xhr, status, data){
+//     console.log('xhr: ', xhr);
+//     console.log('status: ', status);
+//     console.log('data: ', data);
+//   }
+// })
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,72 @@
-// Defining the plugin
+var Bcrypt = require('bcrypt');
+var Joi = require('joi');
+
 exports.register = function(server, options, next){
-  // Define routes
   server.route([
-    // I am **receiving** a POST request
     {
       method: 'POST',
+      path: '/users',
+
+      config: {
+        handler: function(request, reply){
+          var db = request.server.plugins['hapi-mongodb'].db;
+          var user = request.payload.user;
+
+          var uniqUserQuery = {
+            $or: [
+              {username: user.username},
+              {email: user.email}
+            ]
+          };
+
+          db.collection('users').count(uniqUserQuery, function(err, userExist){
+            if (userExist) {
+              return reply('Username already exists', err).code(200);
+            }
+
+            // Encrypt my password
+            Bcrypt.genSalt(5, function(err, salt){
+              Bcrypt.hash(user.password, salt, function(err, encrypted){
+                user.password = encrypted;
+
+                // inserting a user document into DB
+                db.collection('users').insert(user, function(err, writeResult){
+                  if (err) {return reply("Internal MongoDB error", err)}
+
+                  reply(writeResult);
+                });
+              });
+            });
+          });
+        },
+        validate: {
+          payload: {
+            user: {
+              email: Joi.string().email().max(50).required(),
+              password: Joi.string().min(5).max(20).required(),
+              username: Joi.string().min(3).max(20).required(),
+              name: Joi.string() //.min(3).max(20)
+            }
+          }
+        }
+      }
+    },
+    {
+      method: 'GET',
       path: '/users',
       handler: function(request, reply){
         var db = request.server.plugins['hapi-mongodb'].db;
 
-        var user = request.payload.user;
-        // user = {
-        //   name: ...,
-        //   email: ...
-        //   ...
-        // }
+        db.collection('users').find().toArray(function(err, users){
+          if (err) { return reply('Internal MongoDB error', err); }
 
-        db.collection('users').insert(user, function(err, writeResult){
-          reply(writeResult);
+          reply(users);
         });
       }
     }
   ]);
 
-  next(); // <-- DO NOT FORGET THIS
+  next();
 };
 
 // Defining the description of the plugin
